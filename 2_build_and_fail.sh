560314ff6c7db2d4c8aeb79f513affcc2cfef1e4
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-18 12:23:41 +0200

    Check if clone is done

--- 2_build_and_fail.sh
+++ 2_build_and_fail.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ ! -d nf-tower ]
+then
+  echo "ERROR: folder 'nf-tower' not found"
+  echo " "
+  echo "Tip: Run:"
+  echo " "
+  echo "  ./1_clone_repo.sh"
+  echo " "
+fi
+
 (
   cd nf-tower || exit 2
   make build
