33aa7fa5bc8b90965fdce258a9b6d91b1effe30e
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-18 15:32:24 +0200

    Fix Java error

--- run_nft.sh
+++ run_nft.sh
@@ -17,6 +17,18 @@ then
   echo " "
 fi
 
+if [ ! -d ../nextflow_troubleshooting ]
+then
+  echo "ERROR: folder '../nextflow_troubleshooting' not found"
+  echo " "
+  echo "Tip: Run:"
+  echo " "
+  echo "  git clone https://github.com/richelbilderbeek/nextflow_troubleshooting .."
+  echo " "
+fi
+
+../nextflow_troubleshooting/scripts/fix_java_error.sh
+
 (
   cd nf-tower || exit 2
   make run
